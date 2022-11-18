450cce57ef9e90a7beb34c76faaea85d9adfea76
sreeze <117290673+sreeze@users.noreply.github.com>
2022-11-18 14:00:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -458,27 +458,3 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
-async def get_shortlink(link):
-    https = link.split(":")[0]
-    if "http" == https:
-        https = "https"
-        link = link.replace("http", https)
-    url = f'https://dulink.in/api'
-    params = {'api': SHORTENER_API,
-              'url': link,
-              }
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json()
-                if data["status"] == "success":
-                    return data['shortenedUrl']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://dulink.in/api?api={SHORTENER_API}&link={link}'
-
-    except Exception as e:
-        logger.error(e)
-        return f'https://dulink.in/api?api={SHORTENER_API}&link={link}'
